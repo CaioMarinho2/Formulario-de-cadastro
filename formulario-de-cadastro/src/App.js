30c8a3e4246e9f99531cbe1a9ed0c7277c653820
God bless your rotten soul <caionovo10@gmail.com>
2022-06-11 03:22:19 -0300

    identação do codigo

--- formulario-de-cadastro/src/App.js
+++ formulario-de-cadastro/src/App.js
@@ -1,25 +1,23 @@
-
-import './App.css';
-import Form from './components/form';
+import "./App.css";
+import Form from "./components/form";
 import { Switch, Route } from "react-router-dom";
-import Home from './pages/home';
+import Home from "./pages/home";
 
 function App() {
-  return(
+  return (
     <div className="App">
       <section>
-       <Switch>
-       <Route exact path="/">
-        <Form/>
-        </Route>
-        <Route path="/home/:name">
-       <Home/>
-        </Route>
-       </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Form />
+          </Route>
+          <Route path="/home/:name">
+            <Home />
+          </Route>
+        </Switch>
       </section>
     </div>
-  )
+  );
 }
-
 
 export default App;
--- formulario-de-cadastro/src/pages/home/index.jsx
+++ formulario-de-cadastro/src/pages/home/index.jsx
@@ -1,25 +1,29 @@
-import CachorroFeliz from "./images/happy-birthday-smiling-dog.jpg"
-import "./index.css"
+import CachorroFeliz from "./images/happy-birthday-smiling-dog.jpg";
+import "./index.css";
 import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
-
-
-
-
 function Home() {
+  const history = useHistory();
+  const { name } = useParams();
 
-    const history = useHistory();
-    const { name } = useParams();
-  
-    return(
-        <div className="home">
-         <h1>Bem-Vindo, {name}</h1>
-         <img className="cachorroFeliz" alt="Cachorro Feliz" src={CachorroFeliz}></img>
-         <button className="voltar" onClick={()=>{
-             history.push('/')
-         }} >Voltar</button>
-        </div>
-    )
+  return (
+    <div className="home">
+      <h1>Bem-Vindo, {name}</h1>
+      <img
+        className="cachorroFeliz"
+        alt="Cachorro Feliz"
+        src={CachorroFeliz}
+      ></img>
+      <button
+        className="voltar"
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        Voltar
+      </button>
+    </div>
+  );
 }
-export default Home
+export default Home;
